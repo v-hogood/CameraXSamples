8df7c2d12b5ced5bf1b53a5a27956a184a9edf9f
Howard Good <v-hogood@outlook.com>
2021-03-31 22:02:38 -0700

    Remove ProcessingTask.

--- HdrViewfinderX/ViewfinderProcessor.cs
+++ HdrViewfinderX/ViewfinderProcessor.cs
@@ -10,18 +10,18 @@ namespace HdrViewfinder
     //
     // Renderscript-based merger for an HDR viewfinder
     //
-    public class ViewfinderProcessor
+    public class ViewfinderProcessor : Object, Allocation.IOnBufferAvailableListener
     {
         private Allocation mInputHdrAllocation;
         private Allocation mPrevAllocation;
         private Allocation mOutputAllocation;
 
-        private Handler mProcessingHandler;
         private ScriptIntrinsicYuvToRGB mHdrYuvToRGBScript;
         private ScriptIntrinsicColorMatrix mHdrColorMatrixScript;
         private ScriptIntrinsicBlend mHdrBlendScript;
 
-        public ProcessingTask mHdrTask;
+        private Size mDimensions;
+        private int mFrameCounter;
 
         private int mMode;
 
@@ -45,16 +45,13 @@ namespace HdrViewfinder
             mOutputAllocation = Allocation.CreateTyped(rs, rgbTypeBuilder.Create(),
                 AllocationUsage.IoOutput | AllocationUsage.Script);
 
-            HandlerThread processingThread = new HandlerThread("ViewfinderProcessor");
-            processingThread.Start();
-            mProcessingHandler = new Handler(processingThread.Looper);
-
             mHdrYuvToRGBScript = ScriptIntrinsicYuvToRGB.Create(rs, Element.RGBA_8888(rs));
             mHdrColorMatrixScript = ScriptIntrinsicColorMatrix.Create(rs);
             mHdrColorMatrixScript.SetColorMatrix(new Matrix4f(new float[] { 0.5f, 0, 0, 0, 0, 0.5f, 0, 0, 0, 0, 0.5f, 0, 0, 0, 0, 0.5f }));
             mHdrBlendScript = ScriptIntrinsicBlend.Create(rs, Element.RGBA_8888(rs));
 
-            mHdrTask = new ProcessingTask(this, mInputHdrAllocation, dimensions);
+            mDimensions = dimensions;
+            mFrameCounter = 0;
 
             SetRenderMode(ModeNormal);
         }
@@ -66,6 +63,7 @@ namespace HdrViewfinder
 
         public void SetOutputSurface(Surface output)
         {
+            mInputHdrAllocation.SetOnBufferAvailableListener(output == null ? null : this);
             mOutputAllocation.Surface = output;
         }
 
@@ -78,90 +76,45 @@ namespace HdrViewfinder
         // Simple class to keep track of incoming frame count,
         // and to process the newest one in the processing thread
         //
-        public class ProcessingTask : Object, IRunnable, Allocation.IOnBufferAvailableListener
+        public void OnBufferAvailable(Allocation a)
         {
-            private int mPendingFrames = 0;
-            private int mFrameCounter = 0;
-            private Size mDimensions;
+            a.IoReceive();
+            mHdrYuvToRGBScript.SetInput(a);
 
-            private Allocation mInputAllocation;
-
-            private ViewfinderProcessor mParent;
-
-            public ProcessingTask(ViewfinderProcessor parent, Allocation input, Size dimensions)
+            // Run processing pass
+            if (mMode != ModeNormal)
             {
-                mParent = parent;
-                mInputAllocation = input;
-                mInputAllocation.SetOnBufferAvailableListener(this);
-                mDimensions = dimensions;
-            }
-
-            public void OnBufferAvailable(Allocation a)
-            {
-                lock(this)
+                if (mMode == ModeHdr)
                 {
-                    mPendingFrames++;
-                    mParent.mProcessingHandler.Post(this);
-                }
-            }
-
-            public void Run()
-            {
-                // Find out how many frames have arrived
-                int pendingFrames;
-                lock(this)
-                {
-                    pendingFrames = mPendingFrames;
-                    mPendingFrames = 0;
-
-                    // Discard extra messages in case processing is slower than frame rate
-                    mParent.mProcessingHandler.RemoveCallbacks(this);
-                }
-
-                // Get to newest input
-                for (int i = 0; i < pendingFrames; i++)
-                {
-                    mInputAllocation.IoReceive();
-                }
-                mFrameCounter += pendingFrames - 1;
-
-                mParent.mHdrYuvToRGBScript.SetInput(mInputAllocation);
-
-                // Run processing pass
-                if (mParent.mMode != ModeNormal)
-                {
-                    if (mParent.mMode == ModeHdr)
-                    {
-                        mParent.mHdrColorMatrixScript.ForEach(mParent.mPrevAllocation, mParent.mOutputAllocation);
-                    }
-                    else
-                    {
-                        int cutPointX = (mFrameCounter & 1) == 0 ? 0 : mDimensions.Width / 2;
-                        mParent.mOutputAllocation.Copy2DRangeFrom(cutPointX, 0, mDimensions.Width / 2, mDimensions.Height,
-                            mParent.mPrevAllocation, cutPointX, 0);
-                    }
-
-                    mParent.mHdrYuvToRGBScript.ForEach(mParent.mPrevAllocation);
-
-                    if (mParent.mMode == ModeHdr)
-                    {
-                        mParent.mHdrBlendScript.ForEachDstOver(mParent.mPrevAllocation, mParent.mOutputAllocation);
-                    }
-                    else
-                    {
-                        int cutPointX = (mFrameCounter & 1) == 1 ? 0 : mDimensions.Width / 2;
-                        mParent.mOutputAllocation.Copy2DRangeFrom(cutPointX, 0, mDimensions.Width / 2, mDimensions.Height,
-                            mParent.mPrevAllocation, cutPointX, 0);
-                    }
+                    mHdrColorMatrixScript.ForEach(mPrevAllocation, mOutputAllocation);
                 }
                 else
                 {
-                    mParent.mHdrYuvToRGBScript.ForEach(mParent.mOutputAllocation);
+                    int cutPointX = (mFrameCounter & 1) == 0 ? 0 : mDimensions.Width / 2;
+                    mOutputAllocation.Copy2DRangeFrom(cutPointX, 0, mDimensions.Width / 2, mDimensions.Height,
+                        mPrevAllocation, cutPointX, 0);
                 }
-                mFrameCounter++;
 
-                mParent.mOutputAllocation.IoSend();
+                mHdrYuvToRGBScript.ForEach(mPrevAllocation);
+
+                if (mMode == ModeHdr)
+                {
+                    mHdrBlendScript.ForEachDstOver(mPrevAllocation, mOutputAllocation);
+                }
+                else
+                {
+                    int cutPointX = (mFrameCounter & 1) == 1 ? 0 : mDimensions.Width / 2;
+                    mOutputAllocation.Copy2DRangeFrom(cutPointX, 0, mDimensions.Width / 2, mDimensions.Height,
+                        mPrevAllocation, cutPointX, 0);
+                }
             }
+            else
+            {
+                mHdrYuvToRGBScript.ForEach(mOutputAllocation);
+            }
+            mFrameCounter++;
+
+            mOutputAllocation.IoSend();
         }
     }
 }
