e61fbb070f746cd4cf122210669ee62d51a5f421
Howard Good <v-hogood@outlook.com>
2021-11-30 20:42:16 -0800

    Add files via upload
    created a placeholder for CameraXVideo

---
+++ CameraXVideo/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace CameraXVideo
+{
+    [Activity(Name = "com.android.example.cameraxvideo.MainActivity", Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private FrameLayout container;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+            container = FindViewById(Resource.Id.fragment_container) as FrameLayout;
+
+            // Fix the screen orientation for this sample to focus on cameraX API
+            // rather than UI
+            RequestedOrientation = ScreenOrientation.Portrait;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            // Before setting full screen flags, we must wait a bit to let UI settle; otherwise, we may
+            // be trying to set app to immersive mode before it's ready and the flags do not stick
+            container.PostDelayed(() => container.SystemUiVisibility =
+                (Android.Views.StatusBarVisibility)FlagsFullscreen,
+                ImmersiveFlagTimeout);
+        }
+
+        // Combination of all flags required to put activity into immersive mode
+        private const int FlagsFullscreen =
+            (int)SystemUiFlags.LowProfile |
+            (int)SystemUiFlags.Fullscreen |
+            (int)SystemUiFlags.LayoutStable |
+            (int)SystemUiFlags.ImmersiveSticky;
+
+        // Milliseconds used for UI animations
+        public const long AnimationFastMillis = 50L;
+        public const long AnimationSlowMillis = 100L;
+        private const long ImmersiveFlagTimeout = 500L;
+    }
+}
